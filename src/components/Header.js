a7ab9c1c1b35132d02fecacdbcdf4823ab6d7cae
agayen <abhijitagyen428@gmail.com>
2021-12-11 23:39:22 +0530

    third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { FaHome,FaBriefcase,FaList,FaAddressBook } from 'react-icons/fa';
+import { BsPersonSquare } from 'react-icons/bs';
+
+
 
 export default function Header() {
     return (
         <>      
 <nav className="navbar" >
-    <a href="/#" className="active"> <i className="fa fa-home"></i> <span>home</span> </a>
-    <a href="/#about"> <i className="fa fa-user"></i> <span>about</span> </a>
-    <a href="/#work"> <i className="fa fa-briefcase"></i> <span>My Work</span></a>
-    <a href="/#"> <i className="fa fa-list"></i> <span>blogs</span> </a>
-    <a href="/#contact"> <i className="fa fa-address-book"></i> <span>contact</span> </a>
+    <a href="/#"> <FaHome className="nav_icon"/><span>home</span> </a>
+    <a href="/#about"><BsPersonSquare className="nav_icon" /><span>about</span> </a>
+    <a href="/#work"><FaBriefcase className="nav_icon"/><span>My Work</span></a>
+    <a href="/#"><FaList className="nav_icon"/><span>blogs</span> </a>
+    <a href="/#contact"><FaAddressBook className ="nav_icon"/><span>contact</span> </a>
 </nav>
         </>
     )
